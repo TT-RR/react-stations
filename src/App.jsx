d0e95aec9a15cd5b1ca76db41a7ac350271bdadd
TT-RR <kaiza1mage@gmail.com>
2024-09-06 12:00:56 +0900

    Station 3 クリア

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  let [dogUrl, setdogUrl] = useState('https://images.dog.ceo/breeds/terrier-westhighland/n02098286_4120.jpg');
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>Dogアプリ</header>
+      <p>犬の画像を表示するサイトです」</p>
+      <img src={dogUrl} alt='dog' />
     </div>
   )
 }
