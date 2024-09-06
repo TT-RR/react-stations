41aff9b6b8c8bb8f87785f1e3d05af3279a28cc4
TT-RR <kaiza1mage@gmail.com>
2024-09-06 12:15:22 +0900

    Station5 ボタンクリックで画像変更

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,17 @@ import { useState } from 'react'
 export const App = () => {
   let [dogUrl, setdogUrl] = useState('https://images.dog.ceo/breeds/terrier-westhighland/n02098286_4120.jpg');
 
+  function upDatedogUrl() {
+    setdogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg');
+  }
+
   return (
     <div>
       <header>Dogアプリ</header>
-      <p>犬の画像を表示するサイトです」</p>
+      <p>犬の画像を表示するサイトです</p>
       <img src={dogUrl} alt='dog' />
+      <br />
+      <button onClick={upDatedogUrl}>更新</button>
     </div>
   )
 }
