d00ec576ab7d3d4b72f032d7c1178aa667f5aaff
TT-RR <kaiza1mage@gmail.com>
2024-09-09 10:44:50 +0900

    Station9 useEffect

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,6 +1,23 @@
 // @ts-check
+import { useState, useEffect } from 'react'
 
 export const DogListContainer = () => {
+  const [breeds, setBreeds] = useState([""]);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((response) => response.json())
+      .then((data) => {
+        const breedList = Object.keys(data.message)
+        setBreeds(breedList)
+      })
+      .catch((error) => {
+        console.error('エラー:', error);
+      })
+  }, []);
+
+  console.log(breeds)
+
   return <></>
 }
 
